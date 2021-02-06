130493138de255ac2276b9139259fab15551ff34
Nicolo <71701653+FAllensex@users.noreply.github.com>
2021-02-06 09:45:25 +0400

    Add files via upload

---
+++ Frace Code Editor/Frace Code Editor/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Frace_Code_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+          
+            ofd.Title = "open a file";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(ofd.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+
+              
+                sr.Close();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                        
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.Filter = ".txt|*.txt";
+            svf.Title = "save a file";
+            
+            if (svf.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = svf.FileName;
+                BinaryWriter bw = new BinaryWriter(File.Create(path));
+                bw.Write(richTextBox1.Text);
+                bw.Dispose();
+            }
+
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //for ctrl + s
+            if (e.KeyData == Keys.ControlKey && e.Modifiers == Keys.S)
+            {
+
+                SaveFileDialog svf1 = new SaveFileDialog();
+                svf1.Filter = ".txt|*.txt";
+                svf1.Title = "save a file";
+                if (svf1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string path = svf1.FileName;
+                    BinaryWriter bw1 = new BinaryWriter(File.Create(path));
+                    bw1.Write(richTextBox1.Text);
+                    bw1.Dispose();
+                }
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
